5664adb6d159898e31cfa5bea61ad4ac96fb175c
dhaldane <dhaldane@berkeley.edu>
2013-02-21 22:53:22 -0800

    added pid telemetry

--- pid-ip2.5.c
+++ pid-ip2.5.c
@@ -217,6 +217,52 @@ void pidSetGains(int pid_num, int Kp, int Ki, int Kd, int Kaw, int ff){
 	pidObjs[pid_num].feedforward = ff;
 }
 
+//============================Telemetry==================================================
+#define	CMD_PID_TELEMETRY	0x83
+
+int samplesToSave;
+int gdata[3];	//gyrodata
+int xldata[3];  // accelerometer data 
+
+// structure to keep track of telemetry recording
+
+extern pidT steeringPID;
+extern pidPos pidObjs[NUM_PIDS];
+extern int bemf[NUM_PIDS];
+
+telemU telemPIDdata;
+
+// store current PID info into structure. Used by telemSaveSample and CmdGetPIDTelemetry
+void telemGetPID()
+{
+	telemPIDdata.telemStruct.timeStamp = (long)sclockGetTime(); 
+
+// since T1 has higher priority, these state readings might get interrupted 
+	CRITICAL_SECTION_START  // need coherent sample without T1 int updates
+//  save Hall encoder position instead of commanded thrust
+		telemPIDdata.telemStruct.posL = pidObjs[0].p_state;
+		telemPIDdata.telemStruct.posR = pidObjs[1].p_state;
+	// save output instead of reading PWM (sync issue?)
+		telemPIDdata.telemStruct.dcL = pidObjs[0].output;	// left
+		telemPIDdata.telemStruct.dcR = pidObjs[1].output;	// right
+		telemPIDdata.telemStruct.bemfL = bemf[0];
+		telemPIDdata.telemStruct.bemfR = bemf[1];
+	CRITICAL_SECTION_END
+
+		mpuBeginUpdate();
+		mpuGetGyro(&gdata);
+		mpuGetXl(&xldata);
+
+   		telemPIDdata.telemStruct.gyroX = gdata[0];
+		telemPIDdata.telemStruct.gyroY = gdata[1];
+		telemPIDdata.telemStruct.gyroZ = gdata[2]; 
+		telemPIDdata.telemStruct.accelX = xldata[0];
+		telemPIDdata.telemStruct.accelY = xldata[1];
+		telemPIDdata.telemStruct.accelZ = xldata[2];
+		telemPIDdata.telemStruct.Vbatt = (int) adcGetVbatt();
+		return;
+}
+
 void pidOn(int pid_num){
 	pidObjs[pid_num].onoff = 1;
 	t1_ticks = 0;
@@ -313,11 +359,14 @@ void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void) {
     LED_3 = 1;
     interrupt_count++;
     if(interrupt_count == 3 && !uart_tx_flag) {
-        uart_tx_packet = ppoolRequestFullPacket(0);
+        telemGetPID();
+        uart_tx_packet = ppoolRequestFullPacket(sizeof(telemStruct_t));
         if(uart_tx_packet != NULL) {
-            paySetType(uart_tx_packet->payload, 'P');
+        	//time|Left pstate|Right pstate|Commanded Left pstate| Commanded Right pstate|DCR|DCL|RBEMF|LBEMF|Gyrox|Gyroy|Gyroz|Ax|Ay|Az
+        	//bytes: 4,4,4,4,4,2,2,2,2,2,2,2,2,2,2
+            paySetType(uart_tx_packet->payload, 'CMD_PID_TELEMETRY');
             paySetStatus(uart_tx_packet->payload, ':');
-            paySetData(uart_tx_packet->payload, 0, NULL);
+            paySetData(uart_tx_packet->payload, sizeof(telemStruct_t), (unsigned char *) &telemPIDdata);
             uart_tx_flag = 1;
         }
     } else if(interrupt_count == 4) {
